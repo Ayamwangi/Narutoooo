9104784fd623e89f652cad7eb0822de77d6cb4ee
Ayamwangi <140218952+Ayamwangi@users.noreply.github.com>
2023-08-19 04:56:18 +0700

    Add files via upload

---
+++ tugaspr.py
@@ -0,0 +1,3 @@
+print("Ahmad Fauzan")
+print("X RPL 1")
+print ("cita cita saya adalah developer game dan web designner dan saya ingin sekali mempelajarinya dan karena itu saya masuk ke jurusan rpl")
